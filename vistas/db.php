0d2bbde9b59ddd1d8db0139ad2154487812eaec2
EduardoVega86 <edu.vega86@gmail.com>
2024-03-11 11:37:16 -0500

    Update db.php

--- vistas/db.php
+++ vistas/db.php
@@ -1,5 +1,5 @@
 <?php
 @define('DB_HOST', 'localhost'); //DB_HOST:  generalmente suele ser "127.0.0.1"
-@define('DB_USER', 'root'); //Usuario de tu base de datos
+@define('DB_USER', 'u328397628_cargoxpress'); //Usuario de tu base de datos
 @define('DB_PASS', ''); //Contraseña del usuario de la base de datos
-@define('DB_NAME', 'cushicondor2'); //Nombre de la base de datos
+@define('DB_NAME', 'u328397628_cargoxpress'); //Nombre de la base de datos
